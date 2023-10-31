424066f4caf3bff9cbc5c713561bece4f6ad34f4
wnso-kim <wnso.kim@gmail.com>
2023-10-31 16:53:58 +0900

    feat: spring boot start file 추가

---
+++ src/test/java/com/perfumepedia/PerfumePedia/PerfumePediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfumepedia.PerfumePedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfumePediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
