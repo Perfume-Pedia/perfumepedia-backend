b6227fe7524cdeb5675413e3dbba54e6ba6dc2de
wnso-kim <wnso.kim@gmail.com>
2023-11-01 23:42:09 +0900

    feat: MockAPi를 위한 MockObject 추가
    1. MockPerfume
    2. MockNote

---
+++ src/main/java/MockObject/MockNote.java
@@ -0,0 +1,15 @@
+package MockObject;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class MockNote {
+    private String note_name;
+
+    public MockNote(String note_name){
+        this.note_name = note_name;
+    }
+
+}
---
+++ src/main/java/MockObject/MockPerfume.java
@@ -0,0 +1,50 @@
+package MockObject;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class MockPerfume {
+
+    private String name;
+    private String brand_name;
+    private String price;
+    private String url;
+    private String image_url;
+    private String brand_url;
+
+    private List<MockNote> top_note_list;
+    private List<MockNote> middle_note_list;
+    private List<MockNote> base_note_list;
+    private List<MockNote> single_note_lis;
+
+    public MockPerfume() {
+
+        top_note_list = new ArrayList<>();
+        middle_note_list = new ArrayList<>();
+        base_note_list = new ArrayList<>();
+        single_note_lis = new ArrayList<>();
+
+    }
+
+    public void addTopNote(MockNote note){
+        top_note_list.add(note);
+    }
+
+    public void addMiddleNote(MockNote note){
+        middle_note_list.add(note);
+    }
+
+    public void addBaseNote(MockNote note){
+        base_note_list.add(note);
+    }
+
+    public void addSingleNote(MockNote note){
+        single_note_lis.add(note);
+    }
+}
