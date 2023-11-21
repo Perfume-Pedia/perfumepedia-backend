a526fc34120c868a147033fa70e66aa0f9046119
김경은 <109902932+KyeongzowD@users.noreply.github.com>
2023-11-22 05:53:40 +0900

    검색 결과 컨트롤러 함수 등록

--- src/main/java/com/perfumepedia/PerfumePedia/controller/SearchResultController.java
+++ src/main/java/com/perfumepedia/PerfumePedia/controller/SearchResultController.java
@@ -3,18 +3,43 @@ package com.perfumepedia.PerfumePedia.controller;
 import com.perfumepedia.PerfumePedia.dto.PerfumeDetailDto;
 import com.perfumepedia.PerfumePedia.dto.SearchResultDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/searchresult")
+@RequestMapping("/api/")
 public class SearchResultController {
 
     //WordService WordService=new WordServise();
 
-    SearchResultDto searchResultDto = new SearchResultDto();
-    PerfumeDetailDto perfumeDetailDto = new PerfumeDetailDto();
+    SearchResultDto searchResultDto ;
+    PerfumeDetailDto perfumeDetailDto ;
+
+    @GetMapping("search")
+    public SearchResultDto getSearchResultDto(){
+
+        return searchResultDto;
+    }
+
+    @GetMapping("search/advanced")
+    public PerfumeDetailDto getPerfumeDetailDto(){
+
+        return perfumeDetailDto;
+    }
+
+    @GetMapping("favperfume")
+    public SearchResultDto getFavPerfumeResultDto(){
+
+        return searchResultDto;
+    }
+
+    @GetMapping("favperfume/advanced")
+    public PerfumeDetailDto getFavPerfumeDetailDto(){
+
+        return perfumeDetailDto;
+    }
 
 
 }
