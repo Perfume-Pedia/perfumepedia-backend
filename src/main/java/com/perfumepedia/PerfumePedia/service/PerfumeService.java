7f00ecaadc2ff29454bd2cb9c40398000400ed65
kjwoooo <kjw1111k@naver.com>
2023-11-27 02:05:15 +0900

    Faet: Service 작성 (11/27)

--- src/main/java/com/perfumepedia/PerfumePedia/service/PerfumeService.java
+++ src/main/java/com/perfumepedia/PerfumePedia/service/PerfumeService.java
@@ -6,10 +6,14 @@ import com.perfumepedia.PerfumePedia.repository.BrandRepository;
 import com.perfumepedia.PerfumePedia.repository.PerfumeNoteRepository;
 import com.perfumepedia.PerfumePedia.repository.PerfumeRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+//import static org.apache.catalina.authenticator.jaspic.PersistentProviderRegistrations.log;
 
 @Service
+
+@Slf4j
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class PerfumeService {
@@ -26,21 +30,30 @@ public class PerfumeService {
      * @param perfume Perfume 엔티티
      * @return 저장한(save or update or nothing) perfume id
      */
+    //@Slf4j //log
     @Transactional(readOnly = false)
     public Long savePerfume(Perfume perfume){
-        // 데이터베이스에 값이 존재하지 않는 경우
+        try {
+            validateDuplicatePerfume(perfume);// 중복 검사
+
+        } catch (IllegalStateException e) {
+            // 데이터베이스에 값이 존재하고, 변경 사항이 있는 경우
+            // updatePerfume 호출
+            log.info(e.getMessage());
+            return updatePerfume(perfume); // 업데이트 한 경우 기존 perfume의 id return
+        }
+
         // PerfumeRepository #save 이용 저장
+        perfumeRepository.save(perfume);
+        return perfume.getId();   // 저장한 경우 perfume의 id return
 
-        // 데이터베이스에 값이 존재하고, 변경 사항이 있는 경우
-        // updatePerfume 호출
+    }
 
-        // 데이터베이스에 값이 존재하고, 변경 사항이 없는 경우
-        // 처리하지 않음
-
-        // 저장한 경우 perfume의 id return
-        // 업데이트 한 경우 기존 perfume의 id return
-        // nothing 인 경우 기존 perfume의 id return
-        return null;
+    private void validateDuplicatePerfume(Perfume perfume) {
+        perfumeRepository.findByName(perfume.getName())
+                .ifPresent(p->{
+                    throw new IllegalStateException("이미 존재하는 향수입니다.");
+                });
     }
 
     /**
@@ -51,10 +64,36 @@ public class PerfumeService {
      */
     @Transactional(readOnly = false)
     public Long updatePerfume(Perfume perfume){
+
+        Perfume existingPerfume = perfumeRepository.findByName(perfume.getName())
+                .orElseThrow(NullPointerException::new);
+
         // Domain #set 이용 변경사항 수정
         // price, url, discontinue, updatedAt만 수정 가능
+        existingPerfume.setPrice(perfume.getPrice());
+        existingPerfume.setUrl(perfume.getUrl());
+        existingPerfume.getDbDate().setUpdatedAt(perfume.getDbDate().getUpdatedAt());
 
         // update한 객체 id 반환
-        return null;
+        return existingPerfume.getId();
     }
+
+    //@Slf4j //log
+    @Transactional(readOnly = false)
+    public void updateDiscontinue(Perfume perfume){
+        try {
+            validateNonExistentPerfume(perfume);//중복검사
+        } catch (IllegalStateException e) {//해당 함수가 존재하지 않을 경우
+        }
+        Perfume existingPerfume = perfumeRepository.findByName(perfume.getName())
+                .orElseThrow(NullPointerException::new);
+
+        existingPerfume.setDiscontinue();
+    }
+    private void validateNonExistentPerfume(Perfume perfume) {
+        perfumeRepository.findByName(perfume.getName())
+                .orElseThrow(() -> new IllegalStateException("존재하지 않는 향수입니다."));
+    }
+
+
 }
