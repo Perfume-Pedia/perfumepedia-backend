eaff6df97b13e90a212aa8f320664b7b77e03a9c
김경은 <109902932+KyeongzowD@users.noreply.github.com>
2023-11-27 03:14:47 +0900

    controller 수정

--- src/main/java/com/perfumepedia/PerfumePedia/controller/SearchResultController.java
+++ src/main/java/com/perfumepedia/PerfumePedia/controller/SearchResultController.java
@@ -1,52 +1,70 @@
 package com.perfumepedia.PerfumePedia.controller;
 
 import com.perfumepedia.PerfumePedia.dto.PerfumeDetailDto;
+import com.perfumepedia.PerfumePedia.dto.ResponseData;
 import com.perfumepedia.PerfumePedia.dto.SearchResultDto;
+import com.perfumepedia.PerfumePedia.service.PerfumeDetailService;
+import com.perfumepedia.PerfumePedia.service.SearchResultService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/")
 public class SearchResultController {
 
+    private SearchResultService searchResultService;
 
-//    private fianl WordService WordService;
-//
-//    @Autowired
-//    public SearchResultController(WordService wordService){
-//        this.wordService=wordService;
-//    }
+    @Autowired
+    public SearchResultController(SearchResultService searchResultService) {
+        this.searchResultService=searchResultService;
+    }
 
-    SearchResultDto searchResultDto ;
-    PerfumeDetailDto perfumeDetailDto ;
 
+    private PerfumeDetailService perfumeDetailService;
+
+    @Autowired
+    public SearchResultController(PerfumeDetailService perfumeDetailService){
+        this.perfumeDetailService=perfumeDetailService;
+    }
+
+
+    //향수 검색시 실행하는 함수
     @GetMapping("search")
-    public SearchResultDto getSearchResultDto(){
-
-        return searchResultDto;
+    public ResponseData getSearchResultDto(
+            @RequestParam(value = "keyword") String keyword
+    ){
+        return searchResultService.searchByKeyword(keyword);
     }
 
-    @GetMapping("search/advanced")
-    public PerfumeDetailDto getPerfumeDetailDto(){
-
-        return perfumeDetailDto;
-    }
-
-    @GetMapping("favperfume")
-    public SearchResultDto getFavPerfumeResultDto(){
-
-        return searchResultDto;
-    }
-
-    @GetMapping("favperfume/advanced")
-    public PerfumeDetailDto getFavPerfumeDetailDto(){
-
-        return perfumeDetailDto;
-    }
+//    //검색한 향수에서 향수 상세 정보 출력하는 함수
+//    @GetMapping("search/advanced")
+//    public ResponseData getPerfumeDetailDto(
+//            @RequestParam(value = "perfumeId") Long perfumeId
+//    ){
+//        return PerfumeDetailService.searchPerfumeWithDetail(Long perfumeId);
+//    }
+//
+//    //선호 향수(마이페이지) 출력 함수
+//    @GetMapping("favperfume")
+//    public ResponseData getFavPerfumeResultDto(){
+//
+//        return searchResultDto;
+//    }
+//
+//    //선호 향수에서 향수 상세 정보 출력하는 함수
+//    @GetMapping("favperfume/advanced")
+//    public ResponseData getFavPerfumeDetailDto(
+//            @RequestParam(value = "perfumeId") Long perfumeId
+//    ){
+//        return PerfumeDetailService.searchPerfumeWithDetail(Long perfumeId);
+//    }
 
 
 }
