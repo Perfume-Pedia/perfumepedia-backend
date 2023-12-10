866e0a60d686ad36200f81bc823b6e849f358fde
wnso-kim <wnso.kim@gmail.com>
2023-12-10 22:20:20 +0900

    refactor > WordService #saveWord DB에 이미 값 존재하는 경우 예외처리 진행
    try catch 문 수정

--- src/test/java/com/perfumepedia/PerfumePedia/service/PerfumeResultServiceTest.java
+++ src/test/java/com/perfumepedia/PerfumePedia/service/PerfumeResultServiceTest.java
@@ -1,6 +1,7 @@
 package com.perfumepedia.PerfumePedia.service;
 
 
+import com.perfumepedia.PerfumePedia.domain.*;
 import com.perfumepedia.PerfumePedia.dto.PerfumeResult;
 import com.perfumepedia.PerfumePedia.dto.ResponseData;
 import com.perfumepedia.PerfumePedia.dto.SearchResultDto;
@@ -30,13 +31,116 @@ public class PerfumeResultServiceTest {
     @Autowired SearchResultService searchResultService;
     @Autowired EntityManager em;
 
+    private void createPerfume(){
+        for(int i=0; i<40; i++){
+            Perfume perfume = new Perfume("perfumeName"+i);
+            Brand brand = new Brand("brandName"+i);
+            Note note = new Note("noteName"+i);
+            PerfumeNote perfumeNote = new PerfumeNote(NoteType.TOP);
+
+            // perfume #set
+            perfume.setBrand(brand);
+            perfume.setUrl("test.perfume.url.com");
+            perfume.setPrice(10000);
+            perfume.setDbDate("2023-12-08");
+            perfume.setImage("image_name", "test/image/path");
+
+            // brand #set
+            brand.setUrl("test.brand.url.com");
+            brand.setDbDate("2023-12-08");
+
+            // note #set
+            note.setDbDate("2023-12-08");
+
+            // perfumeNote #set
+            perfumeNote.setNote(note);
+            perfumeNote.setPerfume(perfume);
+            perfumeNote.setDbDate("2023-12-08");
+
+            brandService.saveBrand(brand);
+            perfumeService.savePerfume(perfume);
+            noteService.saveNote(note);
+            perfumeNoteService.savePerfumeNote(perfumeNote);
+            createWord(perfume, perfume.getName(), WordType.PERFUME);
+            createWord(brand, brand.getName(), WordType.BRAND);
+            createWord(note, note.getName(), WordType.NOTE);
+        }
+    }
+
+
+    private void createWord(Perfume perfume, String name, WordType wordType){
+        int len = name.length();
+
+        for(int i=0; i<len; i++){
+            for(int j=i+1; j<len; j++){
+                Word word = new Word(name.substring(i,j), name, wordType);
+                word.setEntity(perfume);
+                word.setDbDate("2023-12-08");
+                wordService.saveWord(word);
+            }
+        }
+    }
+
+    private void createWord(Brand brand, String name, WordType wordType){
+        int len = name.length();
+
+        for(int i=0; i<len; i++){
+            for(int j=i+1; j<len; j++){
+                Word word = new Word(name.substring(i,j), name, wordType);
+                word.setEntity(brand);
+                word.setDbDate("2023-12-08");
+                wordService.saveWord(word);
+            }
+        }
+    }
+
+    private void createWord(Note note, String name, WordType wordType){
+        int len = name.length();
+
+        for(int i=0; i<len; i++){
+            for(int j=i+1; j<len; j++){
+                Word word = new Word(name.substring(i,j), name, wordType);
+                word.setEntity(note);
+                word.setDbDate("2023-12-08");
+                wordService.saveWord(word);
+            }
+        }
+    }
+
+    @Test
+    public void 향수이름으로_검색() throws Exception{
+        //given
+        createPerfume();
+
+        Long lastId = 0l;
+        int size = 6;
+        String keyword = "perfumeName";
+
+        //when
+        ResponseData responseData = searchResultService.searchByKeyword(lastId, size, keyword);
+        SearchResultDto searchResultDto = (SearchResultDto) responseData.data();
+        List<PerfumeResult> perfumeResults = searchResultDto.getItems();
+
+        //then
+        assertNotNull(responseData);
+        assertNotNull(searchResultDto);
+        assertNotNull(perfumeResults);
+
+//        assertEquals("5", searchResultDto.getLast_item_id());
+//        assertEquals(5, perfumeResults.size());
+    }
+
+
     @Test
     public void 선호향수_검색() throws Exception{
         //given
+        createPerfume();
+
         List<Long> ids = new ArrayList<>();
-        ids.add(12l);
-        ids.add(33l);
-        ids.add(21l);
+        ids.add(23l);
+        ids.add(31l);
+        ids.add(11l);
+        ids.add(123l);
 
         //when
         ResponseData responseData = searchResultService.searchByPerfumeId(ids);
@@ -48,8 +152,8 @@ public class PerfumeResultServiceTest {
         assertNotNull(searchResultDto);
         assertNotNull(perfumeResults);
 
-        assertEquals("", searchResultDto.getLast_item_id());
-        assertEquals(0, perfumeResults.size());
+        assertEquals("11", searchResultDto.getLast_item_id());
+        assertEquals(3, perfumeResults.size());
 
 
     }
