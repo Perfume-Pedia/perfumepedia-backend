ec45a4ff757efedee4a798ff21eb7c65a2af187c
wnso-kim <wnso.kim@gmail.com>
2023-12-25 23:30:09 +0900

    Refactor > Swagger 멘트 추가

--- src/main/java/com/perfumepedia/PerfumePedia/controller/AutoCompleteController.java
+++ src/main/java/com/perfumepedia/PerfumePedia/controller/AutoCompleteController.java
@@ -2,6 +2,8 @@ package com.perfumepedia.PerfumePedia.controller;
 
 import com.perfumepedia.PerfumePedia.dto.ResponseData;
 import com.perfumepedia.PerfumePedia.service.AutoCompleteService;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -22,7 +24,9 @@ public class AutoCompleteController {
 
 
 
-//    검색어 자동완성 컨트롤
+    //    검색어 자동완성 컨트롤
+    @Tag(name = "자동 완성", description = "키워드에 따른 자동완성 결과를 반환하는 API")
+    @Parameter(name = "keyword", description = "키워드 값", example = "딥")
     @GetMapping
     public ResponseData getContainKeyword(
             @RequestParam(value="keyword",required = true) String keyword){
