fa0bb9b4732b1202178348d3943432d113f69968
wnso.kim <wnso.kim@gmail.com>
2024-11-14 15:47:55 +0900

    🐛 Fix: Test 가능하도록 변경

--- src/test/java/com/perfumepedia/perfumepedia/PerfumepediaBackendApplicationTests.java
+++ src/test/java/com/perfumepedia/perfumepedia/PerfumepediaBackendApplicationTests.java
@@ -3,7 +3,7 @@ package com.perfumepedia.perfumepedia;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class PerfumepediaBackendApplicationTests {
 
 	@Test
